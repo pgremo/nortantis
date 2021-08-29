9390c87fbc133a8ca38754a85d3acbc3085756e6
Patrick Gremo <patrick.gremo@yum.com>
2021-08-29 08:59:49 -0500

    cleanup ImageCache

--- src/main/java/nortantis/ImageCache.java
+++ src/main/java/nortantis/ImageCache.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
+import static nortantis.util.ImageHelper.scaleByWidth;
+
 /**
  * Caches icons in memory to avoid recreating or reloading them.
  */
@@ -18,26 +20,23 @@ public class ImageCache
 	/**
 	 * Maps original images, to scaled width, to scaled images.
 	 */
-	private final Map<BufferedImage, Map<Integer, BufferedImage>> scaledCache;
+	private final Map<BufferedImage, Map<Integer, BufferedImage>> scaledCache = new ConcurrentHashMap<>();
 	
 	/**
 	 * Maps file path (or any string key) to images.
 	 */
-	private final Map<String, BufferedImage> fileCache;
+	private final Map<Path, BufferedImage> fileCache = new ConcurrentHashMap<>();
 
 	/**
 	 * Maps string keys to images generated (not loaded directly from files).
 	 */
-	private final Map<String, BufferedImage> generatedImageCache;
+	private final Map<String, BufferedImage> generatedImageCache = new ConcurrentHashMap<>();
 
 	/**
 	 * Singleton
 	 */
 	private ImageCache()
 	{
-		scaledCache = new ConcurrentHashMap<>();
-		fileCache = new ConcurrentHashMap<>();
-		generatedImageCache = new ConcurrentHashMap<>();
 	}
 	
 	public synchronized static ImageCache getInstance()
@@ -52,17 +51,20 @@ public class ImageCache
 		// There is a small chance the 2 different threads might both add the same image at the same time, 
 		// but if that did happen it would only results in a little bit of duplicated work, not a functional
 		// problem.
-		return scaledCache.computeIfAbsent(icon, k -> ((Supplier<Map<Integer, BufferedImage>>) ConcurrentHashMap::new).get()).computeIfAbsent(width, k1 -> ((Supplier<BufferedImage>) () -> ImageHelper.scaleByWidth(icon, width)).get());
+		return scaledCache
+				.computeIfAbsent(icon, k -> new ConcurrentHashMap<>())
+				.computeIfAbsent(width, k1 -> scaleByWidth(icon, width));
 	}
 	
 	public BufferedImage getImageFromFile(Path path)
 	{
-		return fileCache.computeIfAbsent(path.toString(), k -> ((Supplier<BufferedImage>) () -> ImageHelper.read(path)).get());
+		return fileCache
+				.computeIfAbsent(path, k -> ImageHelper.read(path));
 	}
 	
 	public boolean containsImageFile(Path path)
 	{
-		return fileCache.containsKey(path.toString());
+		return fileCache.containsKey(path);
 	}
 	
 	/**
@@ -70,7 +72,7 @@ public class ImageCache
 	 */
 	public BufferedImage getOrCreateImage(String key, Supplier<BufferedImage> createFun)
 	{
-		return generatedImageCache.computeIfAbsent(key.toString(), k -> createFun.get());
+		return generatedImageCache.computeIfAbsent(key, k -> createFun.get());
 	}
 	
 	public static void clear()
